9cd2d3f466a0d3362ebb6ed199e784b07b9cd8a7
Vadzim_Hardzenia <vadimgordenya@gmail.com>
2021-09-23 18:03:11 +0300

    implement redux store using RTK

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import {Link} from "react-router-dom";
+
 const HomePage = () => {
+  const { data, isFetching } = useGetCryptosQuery();
+  const globalStats = data?.data?.stats;
+
+  if (isFetching) {
+    return 'Loading...';
+  }
 
   return (
     <>
@@ -10,22 +19,16 @@ const HomePage = () => {
         Global Crypto Stats
       </Typography.Title>
       <Row>
-        <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value="5" />
-        </Col>
-        <Col span={12}>
-          <Statistic title="Total Exchanges" value="5" />
-        </Col>
-        <Col span={12}>
-          <Statistic title="Total Market Cap" value="5" />
-        </Col>
-        <Col span={12}>
-          <Statistic title="Total 24h Volume" value="5" />
-        </Col>
-        <Col span={12}>
-          <Statistic title="Total Markets" value="5" />
-        </Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
+      <div className="home-heading-container">
+        <Typography.Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Typography.Title>
+        <Typography.Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Typography.Title>
+      </div>
     </>
   );
 };
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': '684c277280mshee9291f8bd35ce5p14e56fjsne4f3015b3920'
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins')
+    })
+  })
+})
+
+export const {
+  useGetCryptosQuery
+} = cryptoApi;
